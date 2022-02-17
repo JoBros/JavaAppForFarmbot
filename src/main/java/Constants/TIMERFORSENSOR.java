3c4ff5bbf5c633db01d5f57fb336800bcb8d72fd
Eugen2586 <40714234+Eugen2586@users.noreply.github.com>
2022-02-17 16:54:49 +0100

    Update TIMERFORSENSOR.java

--- src/main/java/Constants/TIMERFORSENSOR.java
+++ src/main/java/Constants/TIMERFORSENSOR.java
@@ -2,5 +2,5 @@ package Constants;
 
 public class TIMERFORSENSOR {
     public static final long delay = 0;
-    public static final long period = 2000;
+    public static final long period = 20000;
 }
